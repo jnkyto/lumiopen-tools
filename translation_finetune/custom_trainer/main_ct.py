926283ac77984029491a37a4ee5bee9f32c45014
Joona Kytöniemi <jnkyto@utu.fi>
2024-06-05 15:28:07 +0300

    updates to ctrainer

--- translation_finetune/custom_trainer/main_ct.py
+++ translation_finetune/custom_trainer/main_ct.py
@@ -6,11 +6,12 @@ import torch
 import random
 
 from argparse import ArgumentParser
-from accelerate import Accelerator
+from accelerate import Accelerator, logging
 from torch.utils.data import DataLoader
 from torch.optim import AdamW
 from transformers import get_linear_schedule_with_warmup
 from tqdm import tqdm
+from datetime import datetime
 
 from datasets import load_dataset
 from transformers import (
@@ -19,20 +20,21 @@ from transformers import (
     DataCollatorForLanguageModeling,
 )
 
-ACCELERATOR = Accelerator()
-
-DEFAULT_MODEL = 'LumiOpen/Poro-34B'
+accelerator = Accelerator()
+default_model = 'LumiOpen/Poro-34B'
+logger = logging.get_logger(__file__)
 
 
 def argparser():
     ap = ArgumentParser()
     ap.add_argument('--key', default='text')
     ap.add_argument('--verbose', action='store_true')
-    ap.add_argument('--max-length', type=int, default=1024)
+    ap.add_argument('--max_length', type=int, default=1024)
     ap.add_argument("--batch_size", "-b", type=int, default=16)
     ap.add_argument("--epochs", "-e", type=int, default=4)
+    ap.add_argument("--learning_rate", "-r", type=float, default=5e-5)
     ap.add_argument("--dry_run", "-d", action="store_true")
-    ap.add_argument('--model', default=DEFAULT_MODEL)
+    ap.add_argument('--model', default=default_model)
     return ap
 
 
@@ -50,7 +52,9 @@ def prepper(translations):
 
 
 def main(argv):
+    logger.info("Successfully started finetuning script.")
     args = argparser().parse_args(argv[1:])
+    logger.debug(f"{args=}")
     tokenizer = AutoTokenizer.from_pretrained(args.model)
 
     ds = load_dataset("Helsinki-NLP/europarl", "en-fi", split="train")  # With europarl, everything's in "train"
@@ -77,7 +81,7 @@ def main(argv):
     # print(f"{type(data_train_tokenized)}: {data_train_tokenized[0]}")
     # print(f"{type(data_test_tokenized)}: {data_test_tokenized[0]}")
 
-    with ACCELERATOR.main_process_first():
+    with accelerator.main_process_first():
         data_train_tokenized = ds["train"].map(
             preprocess,
             batched=True,
@@ -104,14 +108,15 @@ def main(argv):
     )
 
     if not args.dry_run:
+        # Training arguments
+        num_epochs = args.epochs
+        lr = args.learning_rate
+        gradient_accumulation_steps = 8
+
         model = AutoModelForCausalLM.from_pretrained(
             args.model,
             torch_dtype="auto",
         )
-
-        num_epochs = args.epochs
-        lr = 3e-5
-        gradient_accumulation_steps = 8
 
         optimizer = AdamW(model.parameters(), lr=lr)
         lr_scheduler = get_linear_schedule_with_warmup(
@@ -120,19 +125,22 @@ def main(argv):
             num_training_steps=(len(train_dataloader) * num_epochs)
         )
 
-        model, train_dataloader, test_dataloader, optimizer, lr_scheduler = ACCELERATOR.prepare(
+        model, train_dataloader, test_dataloader, optimizer, lr_scheduler = accelerator.prepare(
             model, train_dataloader, test_dataloader, optimizer, lr_scheduler
         )
 
+        logger.info(f"Starting finetuning.")
+        logger.debug(f"{num_epochs=}, {lr=}, {gradient_accumulation_steps=}")
         for epoch in range(num_epochs):
+            logger.info(f"Epoch {epoch+1} of {num_epochs}.")
             model.train()
             total_loss = 0
-            dl_len = len(train_dataloader)
+
             for step, batch in enumerate(tqdm(train_dataloader)):
                 outputs = model(**batch)
                 loss = outputs.loss
                 total_loss += loss.detach().float()
-                ACCELERATOR.backward(loss)
+                accelerator.backward(loss)
 
                 if step % gradient_accumulation_steps == 0:
                     optimizer.step()
@@ -140,18 +148,23 @@ def main(argv):
                     optimizer.zero_grad()
                     model.zero_grad()
 
-                # capture batch analytics
+                    # Batch analytic capture should go right here
 
+            logger.info(f"Current total loss: {total_loss}.")
+            logger.info("Starting evaluation.")
             model.eval()
             eval_loss = 0
-            dl_len = len(test_dataloader)
             for step, batch in enumerate(tqdm(test_dataloader)):
                 with torch.no_grad():
                     outputs = model(**batch)
                 loss = outputs.loss
                 eval_loss += loss.detach().float()
 
-            model.save_pretrained(f"trained_model-{epoch}")
+            logger.info(f"Evaluation loss: {eval_loss}.")
+            curr_date = datetime.now().isoformat("#", "minutes")
+            saved_model_name = f"trained-e{epoch}-{curr_date}"
+            logger.info(f"Saving trained model as {saved_model_name}.")
+            model.save_pretrained(saved_model_name)
 
 
 if __name__ == '__main__':
