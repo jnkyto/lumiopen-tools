1be94e9a12457a6df1ef265208823cad958f0b95
Joona Kytöniemi <jnkyto@utu.fi>
2024-06-06 12:43:41 +0300

    updates

--- translation_finetune/custom_trainer/main_ct.py
+++ translation_finetune/custom_trainer/main_ct.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # MIT ©2024 Joona Kytöniemi
 
+import os
 import sys
+import csv
 import torch
 import random
+import torch.nn as nn
 
 from argparse import ArgumentParser
 from accelerate import Accelerator
@@ -22,6 +25,7 @@ from transformers import (
 
 accelerator = Accelerator()
 default_model = 'LumiOpen/Poro-34B'
+curr_date = str(datetime.now().isoformat("T", "minutes")).replace(':', '')
 
 
 def argparser():
@@ -33,6 +37,7 @@ def argparser():
     ap.add_argument("--epochs", "-e", type=int, default=4)
     ap.add_argument("--learning_rate", "-r", type=float, default=5e-5)
     ap.add_argument("--dry_run", "-d", action="store_true")
+    ap.add_argument("--seed", "-s", type=int, default=42)
     ap.add_argument('--model', default=default_model)
     return ap
 
@@ -40,14 +45,17 @@ def argparser():
 def prepper(translations):
     template = "<|user|>Käännä suomeksi: {} <|assistant|>"
 
-    new_ds_dict = {
-        "samples": []
-    }
-    for entry in translations["translation"]:
-        entry["en"] = template.format(entry["en"])
-        new_ds_dict["samples"].append(f"{entry['en']}{entry['fi']}")
+    new_ds_data = {"samples": []}
 
-    return new_ds_dict
+    for idx, entry in enumerate(translations["translation"]):
+        entry["en"] = template.format(entry["en"])
+        processed_entry = {
+            "input": entry['en'],
+            "output": entry['fi']
+        }
+        new_ds_data["samples"].append(processed_entry)
+
+    return new_ds_data
 
 
 def main(argv):
@@ -55,13 +63,18 @@ def main(argv):
     tokenizer = AutoTokenizer.from_pretrained(args.model)
 
     ds = load_dataset("Helsinki-NLP/europarl", "en-fi", split="train")  # With europarl, everything's in "train"
-    ds = ds.shuffle(random.seed(5834)).select(range(10000))  # Shuffle dataset and limit sample amount
+    ds = ds.shuffle(random.seed(args.seed)).select(range(10000))  # Shuffle dataset and limit sample amount
     ds = ds.train_test_split(test_size=0.2)
 
     def tokenize(translations):
         for idx, entry in enumerate(translations["samples"]):
-            translations["samples"][idx] = tokenizer(
-                entry,
+            translations["samples"][idx]["input"] = tokenizer(
+                entry["input"],
+                max_length=args.max_length,
+                truncation=True
+            )
+            translations["samples"][idx]["output"] = tokenizer(
+                entry["output"],
                 max_length=args.max_length,
                 truncation=True
             )
@@ -72,11 +85,6 @@ def main(argv):
         prepped_translations = prepper(translations)
         tokenized_translations = tokenize(prepped_translations)
         return tokenized_translations
-
-    # print(data["train"][0])
-    # print(data["test"][0])
-    # print(f"{type(data_train_tokenized)}: {data_train_tokenized[0]}")
-    # print(f"{type(data_test_tokenized)}: {data_test_tokenized[0]}")
 
     with accelerator.main_process_first():
         data_train_tokenized = ds["train"].map(
@@ -89,6 +97,9 @@ def main(argv):
             batched=True,
             load_from_cache_file=False,
         ).remove_columns("translation")["samples"]
+
+    # print(f"{type(data_train_tokenized)}: {data_train_tokenized[0]}")
+    # print(f"{type(data_test_tokenized)}: {data_test_tokenized[0]}")
 
     collator = DataCollatorForLanguageModeling(
         tokenizer=tokenizer,
@@ -126,13 +137,24 @@ def main(argv):
             model, train_dataloader, test_dataloader, optimizer, lr_scheduler
         )
 
+        def append_to_csv(filename, row):
+            exists = os.path.isfile(filename)
+            with open(filename, 'a' if exists else 'w', newline='', encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerow(row)
+
+        def analytics(epoch, split, loss):
+            filename = f"{curr_date}-e{epoch}_analytics.csv"
+            append_to_csv(filename=filename, row=[epoch, split, loss])
+
+        loss_fn = nn.CrossEntropyLoss()
         for epoch in range(num_epochs):
             model.train()
             total_loss = 0
 
             for step, batch in enumerate(tqdm(train_dataloader)):
-                outputs = model(**batch)
-                loss = outputs.loss
+                outputs = model(batch["input"])
+                loss = loss_fn(outputs, batch["output"])
                 total_loss += loss.detach().float()
                 accelerator.backward(loss)
 
@@ -142,17 +164,16 @@ def main(argv):
                     optimizer.zero_grad()
                     model.zero_grad()
 
-                    # Batch analytic capture should go right here
+            analytics(epoch, "train", total_loss)
 
             model.eval()
             eval_loss = 0
             for step, batch in enumerate(tqdm(test_dataloader)):
                 with torch.no_grad():
-                    outputs = model(**batch)
-                loss = outputs.loss
+                    outputs = model(batch["input"])
+                loss = loss_fn(outputs, batch["output"])
                 eval_loss += loss.detach().float()
-
-            curr_date = datetime.now().isoformat("#", "minutes")
+            analytics(epoch, "test", total_loss)
             saved_model_name = f"trained-e{epoch}-{curr_date}"
             model.save_pretrained(saved_model_name)
 
