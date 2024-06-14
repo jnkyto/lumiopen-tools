fc1383888ea2e2a3dcfc3dced176894619c51403
Joona Kytöniemi <jnkyto@utu.fi>
2024-06-14 12:23:59 +0300

    remove end training statement

--- translation_finetune/hf_trainer/main_hf.py
+++ translation_finetune/hf_trainer/main_hf.py
@@ -136,8 +136,6 @@ def main(argv):
             )
             print(f"Fine-tuned model saved in {saved_model_dir}/{saved_model_name}.")
 
-        trainer.accelerator.end_training()
-
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
